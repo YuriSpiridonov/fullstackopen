866eebf99bf2fff2456ab5f103acff8790baaf53
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-08-08 18:13:48 +0200

    Exercises 1.1-1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,28 +1,69 @@
 import React from 'react';
 
-function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Header = (props) => (
+    <h1>{props.name}</h1>
+)
 
+const Part = (props) => {
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h2>{props.parts.name}</h2>
+      <p>There are <u><b>{props.parts.exercises}</b></u> exercises in this part of the course.</p>
     </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part parts={props.parts[0]} />
+      <Part parts={props.parts[1]} />
+      <Part parts={props.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let total = 0;
+  // for (let part of props.parts) { // props.parts.forEach(part => {total += part.exercises;})
+  //   total += part.exercises;
+  // }
+
+  props.parts.forEach(part => {
+    total += part.exercises;
+  })
+  return (
+    <>
+      <p>There are <u><b>{total}</b></u> exercises in total.</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
   )
 }
 
