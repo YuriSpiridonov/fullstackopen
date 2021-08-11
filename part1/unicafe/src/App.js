59f47ec621e7492babbeb40dcd6f2f4771720923
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-08-11 22:01:27 +0200

    Exercises 1.7-1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,42 +1,56 @@
 import React, { useState } from "react";
 
+const Heading = ({ header }) => <h2>{header}</h2>
+
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
   </button>
 )
 
-// const Statistics = (props) => (
+const Statistics = ({ good, neutral, bad }) => {
+  let count =  good + neutral + bad
+  let average = (good - bad) / count
+  let positive = (good / count) * 100
 
-// )
+  if (count === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {count}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  let countAll = 0
 
   const setToGood = (good) => setGood(good)
   const setToNeutral = (neutral) => setNeutral(neutral)
   const setToBad = (bad) => setBad(bad)
-  
-  countAll = good + neutral + bad
 
   return (
     <div>
-      <h2>give feedback</h2>
+      <Heading header='give feedback' />
       <Button handleClick={ () => setToGood(good + 1) } text='good' />
       <Button handleClick={ () => setToNeutral(neutral + 1) } text='neutral' />
       <Button handleClick={ () => setToBad(bad + 1) } text='bad' />
-      
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {countAll}</p>
-      <p>average {(good - bad)/countAll}</p>
-      <p>positive {(good/countAll)*100} %</p>
+
+      <Heading header='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
