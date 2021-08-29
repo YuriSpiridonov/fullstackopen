02db552ad6f1cdf9d09783e71b74645c870f3e9c
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-08-29 14:28:22 +0200

    2.17: Phonebook step9

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -16,4 +16,11 @@ const create = newObject => {
     })
 }
 
-export default { getAll, create }
+const remove = id => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => {
+        return response.data
+    })
+}
+
+export default { getAll, create, remove }
