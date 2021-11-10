f88b509655586f34f1cb9741029fe3df41704873
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-11-10 19:46:48 +0100

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/tests/favoriteBlog.test.js
+++ part4/bloglist/tests/favoriteBlog.test.js
@@ -1,25 +1,13 @@
 const { TestWatcher } = require('@jest/core')
 const listHelper = require('../utils/list_helper')
 
-describe('total likes one blog', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
+describe('favorite blog', () => {
+  const favorite = {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    likes: 12,
+  }
 
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
-  })
-})
-
-describe('total likes six blogs', () => {
   const blogs = [
     {
       _id: '5a422a851b54a676234d17f7',
@@ -71,8 +59,32 @@ describe('total likes six blogs', () => {
     }
   ]
 
-  test('when list has more then one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(36)
+  test('when list has more then one blog and at least one blog has likes more then 0 (zero)', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(favorite)
+  })
+})
+
+describe('favorite blog (zero likes)', () => {
+  const favorite = {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    likes: 0,
+  }
+
+  const blogs = [
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog and 0 (zero) likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(favorite)
   })
 })
