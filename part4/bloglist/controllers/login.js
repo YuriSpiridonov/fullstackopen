45e167a37488eb83b492cdcebd1d0c0eb76d86f7
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-12-01 21:26:28 +0100

    comments and unnecessary outputs are removed

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -10,7 +10,6 @@ loginRouter.post('/', async (request, response) => {
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(body.password, user.passwordHash)
-  console.log(user, body.password, user.passwordHash, passwordCorrect)
 
   if(!(user && passwordCorrect)) {
     return response.status(401).json({
