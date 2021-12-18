5c91bfaf66dee28701fddf5f98b01f2f09fb7dad
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2021-12-18 22:41:15 +0100

    5.2: bloglist frontend, step2

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-const exportGetAll = { getAll }
-
-export default exportGetAll
+export default { getAll, setToken }
