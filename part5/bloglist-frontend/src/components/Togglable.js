cab376ce9f9620ea319fda48d47b5c1139068e93
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2022-01-14 12:55:09 +0100

    5.19: bloglist end to end testing, step3

--- part5/bloglist-frontend/src/components/Togglable.js
+++ part5/bloglist-frontend/src/components/Togglable.js
@@ -20,11 +20,11 @@ const Togglable = React.forwardRef((props, ref) => {
   return (
     <div>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+        <button id='create-new-blog' onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
       <div style={showWhenVisible}>
         {props.children}
-        <button onClick={toggleVisibility}>cancel</button>
+        <button id='cancel' onClick={toggleVisibility}>cancel</button>
       </div>
     </div>
   )
