d4805c1b154283516b7ef83a43d323b26f042af5
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2022-01-26 20:28:04 +0100

    6.1: unicafe revisited, step1

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,59 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('feedbacks are reseted', () => {
+    const goodAction = {
+      type: 'GOOD'
+    }
+    const action = {
+      type: 'ZERO'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newGoodState = counterReducer(state, goodAction)
+    expect(newGoodState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
