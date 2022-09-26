8c7666b75baf89fbb4b52ee83b1542bcf8f4a764
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2022-09-26 21:06:02 +0200

    7.16: Blog view

--- part7/bloglist-frontend/src/components/BlogList.js
+++ part7/bloglist-frontend/src/components/BlogList.js
@@ -1,34 +1,45 @@
+/* eslint-disable */
+
 import React from 'react'
+// import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 
 import Blog from './Blog'
 
-import { likeBlog, deleteBlog } from '../reducers/blogs/blogsReducer'
+// import { likeBlog, deleteBlog } from '../reducers/blogs/blogsReducer'
 
 const BlogList = () => {
   const dispatch = useDispatch()
   const blogs = useSelector((state) => state.blogs)
   const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes)
 
-  const handleLike = async (blog) => {
-    dispatch(likeBlog(blog))
-  }
-  const handleBlogDelete = (blog) => dispatch(deleteBlog(blog))
+  // const handleLike = async (blog) => {
+  //   dispatch(likeBlog(blog))
+  // }
+  // const handleBlogDelete = (blog) => dispatch(deleteBlog(blog))
 
-  const loggedUser = useSelector((state) => state.loggedinUser)
+  // const loggedUser = useSelector((state) => state.loggedinUser)
   return (
-    <div>
-      {sortedBlogs.map((blog) => (
-        <Blog
-          key={blog.id}
-          blog={blog}
-          handleLike={() => handleLike(blog)}
-          handleBlogDelete={() => handleBlogDelete(blog)}
-          loggedUser={loggedUser}
-        />
-      ))}
-    </div>
+    <table>
+      <tbody>
+        {sortedBlogs.map((blog) => (
+          <tr key={blog.id}>
+            <td>
+              <Blog blog={blog} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
+
+// <Blog
+//   key={blog.id}
+//   blog={blog}
+//   handleLike={() => handleLike(blog)}
+//   handleBlogDelete={() => handleBlogDelete(blog)}
+//   loggedUser={loggedUser}
+// />
 
 export default BlogList
