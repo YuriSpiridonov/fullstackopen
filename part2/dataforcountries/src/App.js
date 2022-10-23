1878bf759d7c2f474fd47459c4bf4c2a4fd3b249
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2022-10-23 15:35:03 +0200

    small fixes

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -1,39 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Filter from './components/Filter'
-import Countries from './components/Countries'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
+import Countries from "./components/Countries";
 
 const App = () => {
-  const [ countries, setCountries ] = useState([])
+  const [countries, setCountries] = useState([]);
 
-  const [ filter, setFilter ] = useState('')
+  const [filter, setFilter] = useState("");
 
   const hook = () => {
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => { 
-        setCountries(response.data)
-      })
-  }
+    axios.get("https://restcountries.com/v2/all").then((response) => {
+      setCountries(response.data);
+    });
+  };
 
-  useEffect(hook, [])
+  useEffect(hook, []);
 
   const handleFilter = (event) => {
-    setFilter(event.target.value)
-  }
+    setFilter(event.target.value);
+  };
 
   return (
     <div>
-      <Filter
-        filter={filter}
-        handleFilter={handleFilter}
-      />
-      <Countries
-        countries={countries}
-        filterValue={filter}
-      />
+      <Filter filter={filter} handleFilter={handleFilter} />
+      <Countries countries={countries} filterValue={filter} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
