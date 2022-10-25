c62a53bc7c8d22a7e2246f4e0eada517b9529979
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2022-10-25 13:45:02 +0200

    Exercises 8.8.-8.12. part2

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,16 +1,29 @@
-// import { useState } from "react";
-// import { useQuery } from "@apollo/client";
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
 
-// import { ALL_AUTHORS } from "../queries";
+import { ALL_AUTHORS, EDIT_BIRTHYEAR } from "../queries";
 
 const Authors = (props) => {
-  // console.group("props ", { props });
+  const [name, setName] = useState("");
+  const [born, setBornYear] = useState("");
+
+  const [editAuthor] = useMutation(EDIT_BIRTHYEAR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
   if (!props.show) {
     return null;
   }
-  // const authors = useQuery(ALL_AUTHORS); // eslint-disable-line
-  // .data.allAuthors;
-  // console.log(authors);
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    editAuthor({ variables: { name, born } });
+
+    setName("");
+    setBornYear("");
+  };
+
   const authors = props.authors;
   return (
     <div>
@@ -31,6 +44,44 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <form onSubmit={submit}>
+        <div>
+          <select onChange={({ target }) => setName(target.value)}>
+            {/* All Authors in the list */}
+            {/* {authors.map((a) => (
+              <option key={a.name} value={a.name}>
+                {a.name}
+              </option>
+            ))} */}
+
+            {/* Authors without birth year */}
+            {/* Warning: Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>. */}
+            <option hidden disabled selected value>
+              --- select an author ---
+            </option>
+            {authors.map((a) =>
+              a && !a.born ? (
+                <option key={a.name} value={a.name}>
+                  {a.name}
+                </option>
+              ) : null
+            )}
+          </select>
+        </div>
+        {/* <div>
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div> */}
+        <div>
+          <input
+            value={born}
+            onChange={({ target }) => setBornYear(parseInt(target.value))}
+          />
+        </div>
+        <button type="submit">Change Year</button>
+      </form>
     </div>
   );
 };
