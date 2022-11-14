de7c33b00f72e5357308ba5910dd260c09371e0d
Yuri Spiridonov <48239309+YuriSpiridonov@users.noreply.github.com>
2022-11-14 15:41:53 +0100

    8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,19 +1,67 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
 
+// const Books = async (props) => {
+//   const result = await useQuery(ALL_BOOKS);
+
+const getGenres = (books) => {
+  const genres = [];
+  books.forEach((book) => {
+    book.genres.forEach((genre) => {
+      if (!genres.includes(genre)) {
+        genres.push(genre);
+      }
+    });
+  });
+  return genres;
+};
+
+// const filterBooks = (books, genre) => {
+//   if (genre === "all books") {
+//     return null;
+//   }
+//   return books.reduce((book) => book.genres.includes(genre));
+// };
+
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS);
+  const [selectedGenre, setSelectedGenre] = useState(null);
 
   if (!props.show) {
     return null;
   }
 
   const books = result.data.allBooks;
+  console.log("books ", books);
+  let genres = getGenres(books);
+  // let genres = [];
+  // genres = books.forEach((book) => {
+  //   book.genres.forEach((genre) => {
+  //     if (!genres.includes(genre)) {
+  //       genres.push(genre);
+  //     }
+  //   });
+  // });
+  console.log("genres ", genres);
+
+  const filterBooks = () => {
+    if (!selectedGenre) {
+      return books;
+    }
+    return books.filter((book) => book.genres.includes(selectedGenre));
+  };
 
   return (
     <div>
       <h2>books</h2>
-
+      <div>
+        {selectedGenre ? (
+          <p>
+            in genre <strong>{selectedGenre}</strong>
+          </p>
+        ) : null}
+      </div>
       <table>
         <tbody>
           <tr>
@@ -21,7 +69,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filterBooks(books).map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -30,6 +78,16 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setSelectedGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+        <button key={"null"} onClick={() => setSelectedGenre(null)}>
+          all books
+        </button>
+      </div>
     </div>
   );
 };
